600b6fb1aec111fdb0bbea063ad116e353d940e4
vivek shet <1ms24mc112@gmail.com>
2026-02-04 12:38:32 +0530

    Rename class to MavenAppApplicationTests

--- maven-app(1)/maven-app/src/test/java/com/example/maven_app/MavenAppApplicationTests.java
+++ maven-app(1)/maven-app/src/test/java/com/example/maven_app/MavenAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.example")
-public class MavenAppApplication {
+public class MavenAppApplicationTests {
     public static void main(String[] args) {
         SpringApplication.run(MavenAppApplication.class, args);
     }
